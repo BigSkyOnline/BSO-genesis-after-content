362c75666670f9ecadded1d5237ceee1b05b11da
Bradford Knowlton <brad@ucanbonline.net>
2014-08-07 14:39:43 -0600

    initial commit

---
+++ bso-genesis-after-content.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: BSO Genesis After Content
+Plugin URI: https://github.com/BigSkyOnline/BSO-genesis-after-content/
+Description: Add additional customizations to Genesis Theme.
+Version: 1.8.6
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com/
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Domain Path:       /languages
+Text Domain:       BSO-genesis-after-content
+GitHub Plugin URI: https://github.com/BigSkyOnline/BSO-genesis-after-content
+GitHub Branch:     master
+*/
+
